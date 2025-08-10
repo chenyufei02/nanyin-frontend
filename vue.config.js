bf12dfa64d4f70c54321f4e6990df98bfa097f19
yufei <yufei0205@gmail.com>
2025-08-11 00:20:14 +0800

    0.00 初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8081 // 在这里指定端口号为8081
+  }
 })
