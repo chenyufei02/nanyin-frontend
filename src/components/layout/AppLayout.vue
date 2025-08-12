3eee5f1a05ad24437550be4ad13f8d1c6332831c
yufei <yufei0205@gmail.com>
2025-08-12 12:33:41 +0800

    0.02 登录

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app-layout">
+    <app-navbar />  <app-sidebar /> <div class="main-content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppNavbar from './AppNavbar.vue';
+import AppSidebar from './AppSidebar.vue'; // 确保Sidebar组件已按规范重命名
+
+export default {
+  name: 'AppLayout',
+  components: {
+    AppNavbar,
+    AppSidebar
+  }
+};
+</script>
+
+<style scoped>
+.main-content {
+  margin-top: 60px;
+  margin-left: 240px;
+  padding: 20px;
+}
+</style>
