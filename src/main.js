687fc4c380b089a9a13961a6e9373584fb0f0942
yufei <yufei0205@gmail.com>
2025-08-12 11:14:24 +0800

    0.02

--- src/main.js
+++ src/main.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import './plugins/element.js'
-
-Vue.use(ElementUI);
+import './plugins/element.js' // <-- 确保有且只有这一行关于Element UI的引入
 
 Vue.config.productionTip = false
 
