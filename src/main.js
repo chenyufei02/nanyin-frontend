2f5483d636e7185babd4f47e75b6fb76f315e71e
lly0520 <15545279577@163.com>
2025-08-12 09:28:34 +0800

    登陆页面初步有api问题

--- src/main.js
+++ src/main.js
@@ -5,7 +5,13 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './plugins/element.js'
+//导入全局样式表
+import './assets/css/global.css'
 
+import axios from 'axios'
+//配置请求的根路径 要更改
+axios.defaults.baseURL = 'http://localhost:8081/'
+Vue.prototype.$http = axios
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 8081 // 在这里指定端口号为8081
+    port: 8082 // 在这里指定端口号为8081
   }
 })
