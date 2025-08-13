bc3f7f480735889f33301800373e0449426fed44
lly0520 <15545279577@163.com>
2025-08-13 15:32:13 +0800

    购买基金里面的交易界面

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,19 @@ const routes = [
         meta: { requiresAuth: true } // 【新增】标记这个页面需要登录
       },
       {
+        path: 'my-holdings',
+        name: 'UserHolding',
+        component: () => import('../views/UserHolding.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: 'my-transactions',
+        name: 'UserTransaction',
+        component: () => import('../views/UserTransaction.vue'),
+
+      meta: { requiresAuth: true }
+},
+      {
         path: 'funds',
         name: 'FundList',
         component: () => import('../views/FundList.vue'),
